0a80d042fe3024f55909566609f9133b4aac1241
buildsample <buildsampletest@gmail.com>
2014-09-15 17:52:34 +0530

    Update test.js

--- test.js
+++ test.js
@@ -8,7 +8,7 @@ describe("Index", function () {
     superagent.get("http://localhost:3000/")
       .end(function (e, res) {
         (e === null).should.equal(true);
-        res.text.should.equal("Hey buddy!");
+        res.text.should.equal("Hello buddy!");
         done();
       });
   });
