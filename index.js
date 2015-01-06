ae51324ad029420aff5b73eca07b575abc88d0a3
john stockbauer <john@shippable.com>
2015-01-06 12:18:49 -0800

    added 2nd test and route

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", function (req, res) {
   res.send("Hey buddy!");
 });
 
+app.get("/2", fnction(req, res) {
+  res.send("this is an update")
+});
+
 app.listen(3000, function () {
   console.log('Express listening on port 3000');
 });
