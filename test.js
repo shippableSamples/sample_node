4a5bc554e4c6802971427a5f3391b96777715280
John Stockbauer <john@shippable.com>
2015-01-09 09:59:26 -0800

    make the build fail

--- test.js
+++ test.js
@@ -14,3 +14,13 @@ describe("Index", function () {
   });
 });
 
+describe("Update", function () {
+  it("renders HTML", function (done) {
+    superagent.get("http://localhost:3000/test/")
+      .end(function (e, res) {
+        (e === null).should.equal(true);
+        res.text.should.equal("this is an update FAILURE");
+        done();
+      });
+  });
+});
